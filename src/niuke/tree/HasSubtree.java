988100abeb6320ca14fda74cf3299a4159828f4c
zxliuyu <zxliuyu@didiglobal.com>
2022-01-03 17:22:35 +0800

    feat(*) 牛客网-剑指offer-树相关题目(JZ26 树的子结构)

---
+++ src/niuke/tree/HasSubtree.java
@@ -0,0 +1,23 @@
+package niuke.tree;
+
+import tree.TreeNode;
+
+public class HasSubtree {
+
+    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
+        if (root2 == null || root1 == null) {
+            return false;
+        }
+        return isSubTree(root1, root2) || HasSubtree(root1.left, root2) || HasSubtree(root1.right, root2);
+    }
+    public boolean isSubTree(TreeNode root1, TreeNode root2) {
+        if (root2 == null) {
+            return true;
+        }
+        if (root1 == null || root1.val != root2.val) {
+            return false;
+        }
+        return isSubTree(root1.left, root2.left) && isSubTree(root1.right, root2.right);
+    }
+
+}
