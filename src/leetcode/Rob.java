118cb451937015da264bd6da44a949ad8f7585b7
zxliuyu <zxliuyu@didiglobal.com>
2022-01-18 02:48:30 +0800

    feat(*) leetcode 213. 打家劫舍 II

--- src/leetcode/Rob.java
+++ src/leetcode/Rob.java
@@ -9,8 +9,28 @@ public class Rob {
         dp[0] = nums[0];
         dp[1] = Math.max(nums[0], nums[1]);
         for (int i = 2; i < nums.length; i++) {
-            dp[i] += Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
         return dp[nums.length - 1];
+    }
+
+    public int rob2(int[] nums) {
+        if (nums.length <= 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        return Math.max(robRange(nums, 0, nums.length - 1), robRange(nums, 1, nums.length));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int[] dp = new int[nums.length];
+        dp[start] = nums[start];
+        dp[start] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i < end; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[end - 1];
     }
 }
