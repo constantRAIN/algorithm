8366732a291bcbc6375b7c94bda3e2776d473aa8
zxliuyu <zxliuyu@didiglobal.com>
2022-03-03 00:56:17 +0800

    feat(*) leetcode 213 修复打家劫舍2代码

--- src/leetcode/Rob.java
+++ src/leetcode/Rob.java
@@ -27,7 +27,7 @@ public class Rob {
     public int robRange(int[] nums, int start, int end) {
         int[] dp = new int[nums.length];
         dp[start] = nums[start];
-        dp[start] = Math.max(nums[start], nums[start + 1]);
+        dp[start+1] = Math.max(nums[start], nums[start + 1]);
         for (int i = start + 2; i < end; i++) {
             dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
         }
