5931e5da9768b8c9098f60063b84d8f5b12c55e1
zxliuyu <zxliuyu@didiglobal.com>
2022-03-06 17:23:41 +0800

    feat(*) leetcode 3. 无重复字符的最长子串

--- src/leetcode/LengthOfLongestSubstring3.java
+++ src/leetcode/LengthOfLongestSubstring3.java
@@ -1,5 +1,6 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -24,7 +25,7 @@ public class LengthOfLongestSubstring3 {
 
     public int lengthOfLongestSubstring2(String s) {
         int max = 0;
-        if (s == null || s.equals("")) {
+        if (s.length() == 0) {
             return max;
         }
         HashMap<Character, Integer> map = new HashMap<>();
@@ -32,6 +33,39 @@ public class LengthOfLongestSubstring3 {
         for (int i = 0; i < s.length(); i++) {
             if (map.containsKey(s.charAt(i))) {
                 left = Math.max(left, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            max = Math.max(max, i - left + 1);
+        }
+        return max;
+    }
+
+
+    public int lengthOfLongestSubstring3(String s) {
+        LinkedList<Character> list = new LinkedList<>();
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            while (list.contains(s.charAt(i))){
+                list.removeFirst();
+            }
+            list.addLast(s.charAt(i));
+            max = Math.max(list.size(), max);
+        }
+        return max;
+
+    }
+
+    public int lengthOfLongestSubstring4(String s) {
+        if (s.length() == 0) {
+            return 0;
+        }
+        int max = 0;
+        // key 代表每一个字符，value 代表当前字符的位置
+        HashMap<Character, Integer> map = new HashMap<>();
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(map.get(s.charAt(i)) + 1, left);
             }
             map.put(s.charAt(i), i);
             max = Math.max(max, i - left + 1);
