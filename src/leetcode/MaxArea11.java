fe2bf0975575974fc100dfb35b6ab43d989f48b0
zxliuyu <zxliuyu@didiglobal.com>
2022-03-13 17:30:07 +0800

    feat(*) leetcode 11. 盛最多水的容器

---
+++ src/leetcode/MaxArea11.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+public class MaxArea11 {
+
+    /**
+     * 11. 盛最多水的容器
+     * 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+     *
+     * 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+     *
+     * 返回容器可以储存的最大水量。
+     *
+     * 说明：你不能倾斜容器。
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/container-with-most-water
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1, max = 0;
+        while (left < right) {
+            max = height[left] < height[right] ?
+                    Math.max((right - left) * height[left++], max) :
+                    Math.max((right - left) * height[right--], max);
+        }
+        return max;
+    }
+}
